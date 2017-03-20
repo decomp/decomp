df1b0d184f463227a304060a07b52e15e95c7301
mewmew <rnd0x00@gmail.com>
2017-03-20 09:00:31 +0100

    graphs: Remove package; no longer needed since rewrite of restructure.

--- cfa/primitive/primitive.go
+++ cfa/primitive/primitive.go
@@ -4,16 +4,16 @@ package primitive
 
 // A Primitive represents a high-level control flow primitive (e.g. 2-way
 // conditional, pre-test loop) as a mapping from subgraph (graph representation
-// of a control flow primitive) node names to control flow graph node ID.
+// of a control flow primitive) node names to control flow graph node names.
 type Primitive struct {
 	// Primitive name; e.g. "if", "pre_loop", ...
 	Prim string `json:"prim"`
-	// Node ID of the primitive; e.g. "33".
+	// Node name of the primitive; e.g. "33".
 	Node string `json:"node"`
 	// Node mapping; e.g. {"cond": "17", "body": "24", "exit": "32"}
 	Nodes map[string]string `json:"nodes"`
-	// Entry node ID.
+	// Entry node name.
 	Entry string `json:"entry"`
-	// Exit node ID.
+	// Exit node name.
 	Exit string `json:"exit,omitempty"`
 }
--- graphs/cmd/iso/z_usage.go
+++
@@ -1,8 +0,0 @@
-// Usage:
-//
-//     iso [OPTION]... SUB.dot GRAPH.dot
-//
-// Flags:
-//
-//     -start="": Locate an isomorphism of SUB in GRAPH which starts at the given node.
-package main
