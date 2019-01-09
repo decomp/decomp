2e22cc6cfd6353209b4a44edd598855220993c88
Robin Eklind <rnd0x00@gmail.com>
2019-01-09 19:35:48 +0100

    cmd/go-post: add -assignbinop rule

--- cmd/go-post2/killpos.go
+++ cmd/go-post2/killpos.go
@@ -22,16 +22,25 @@ func killpos(file *ast.File) bool {
 	walk(file, func(n interface{}) {
 		switch n := n.(type) {
 		case *ast.ArrayType:
+			if n == nil {
+				return
+			}
 			if n.Lbrack != token.NoPos {
 				fixed = true
 			}
 			n.Lbrack = token.NoPos
 		case *ast.AssignStmt:
+			if n == nil {
+				return
+			}
 			if n.TokPos != token.NoPos {
 				fixed = true
 			}
 			n.TokPos = token.NoPos
 		case *ast.BadDecl:
+			if n == nil {
+				return
+			}
 			if n.From != token.NoPos {
 				fixed = true
 			}
@@ -41,6 +50,9 @@ func killpos(file *ast.File) bool {
 			}
 			n.To = token.NoPos
 		case *ast.BadExpr:
+			if n == nil {
+				return
+			}
 			if n.From != token.NoPos {
 				fixed = true
 			}
@@ -50,6 +62,9 @@ func killpos(file *ast.File) bool {
 			}
 			n.To = token.NoPos
 		case *ast.BadStmt:
+			if n == nil {
+				return
+			}
 			if n.From != token.NoPos {
 				fixed = true
 			}
@@ -59,16 +74,25 @@ func killpos(file *ast.File) bool {
 			}
 			n.To = token.NoPos
 		case *ast.BasicLit:
+			if n == nil {
+				return
+			}
 			if n.ValuePos != token.NoPos {
 				fixed = true
 			}
 			n.ValuePos = token.NoPos
 		case *ast.BinaryExpr:
+			if n == nil {
+				return
+			}
 			if n.OpPos != token.NoPos {
 				fixed = true
 			}
 			n.OpPos = token.NoPos
 		case *ast.BlockStmt:
+			if n == nil {
+				return
+			}
 			if n.Lbrace != token.NoPos {
 				fixed = true
 			}
@@ -78,11 +102,17 @@ func killpos(file *ast.File) bool {
 			}
 			n.Rbrace = token.NoPos
 		case *ast.BranchStmt:
+			if n == nil {
+				return
+			}
 			if n.TokPos != token.NoPos {
 				fixed = true
 			}
 			n.TokPos = token.NoPos
 		case *ast.CallExpr:
+			if n == nil {
+				return
+			}
 			if n.Lparen != token.NoPos {
 				fixed = true
 			}
@@ -96,6 +126,9 @@ func killpos(file *ast.File) bool {
 			}
 			n.Rparen = token.NoPos
 		case *ast.CaseClause:
+			if n == nil {
+				return
+			}
 			if n.Case != token.NoPos {
 				fixed = true
 			}
@@ -105,6 +138,9 @@ func killpos(file *ast.File) bool {
 			}
 			n.Colon = token.NoPos
 		case *ast.ChanType:
+			if n == nil {
+				return
+			}
 			if n.Begin != token.NoPos {
 				fixed = true
 			}
@@ -114,6 +150,9 @@ func killpos(file *ast.File) bool {
 			}
 			n.Arrow = token.NoPos
 		case *ast.CommClause:
+			if n == nil {
+				return
+			}
 			if n.Case != token.NoPos {
 				fixed = true
 			}
@@ -123,11 +162,17 @@ func killpos(file *ast.File) bool {
 			}
 			n.Colon = token.NoPos
 		case *ast.Comment:
+			if n == nil {
+				return
+			}
 			if n.Slash != token.NoPos {
 				fixed = true
 			}
 			n.Slash = token.NoPos
 		case *ast.CompositeLit:
+			if n == nil {
+				return
+			}
 			if n.Lbrace != token.NoPos {
 				fixed = true
 			}
@@ -137,21 +182,33 @@ func killpos(file *ast.File) bool {
 			}
 			n.Rbrace = token.NoPos
 		case *ast.DeferStmt:
+			if n == nil {
+				return
+			}
 			if n.Defer != token.NoPos {
 				fixed = true
 			}
 			n.Defer = token.NoPos
 		case *ast.Ellipsis:
+			if n == nil {
+				return
+			}
 			if n.Ellipsis != token.NoPos {
 				fixed = true
 			}
 			n.Ellipsis = token.NoPos
 		case *ast.EmptyStmt:
+			if n == nil {
+				return
+			}
 			if n.Semicolon != token.NoPos {
 				fixed = true
 			}
 			n.Semicolon = token.NoPos
 		case *ast.FieldList:
+			if n == nil {
+				return
+			}
 			if n.Opening != token.NoPos {
 				fixed = true
 			}
@@ -161,21 +218,33 @@ func killpos(file *ast.File) bool {
 			}
 			n.Closing = token.NoPos
 		case *ast.File:
+			if n == nil {
+				return
+			}
 			if n.Package != token.NoPos {
 				fixed = true
 			}
 			n.Package = token.NoPos
 		case *ast.ForStmt:
+			if n == nil {
+				return
+			}
 			if n.For != token.NoPos {
 				fixed = true
 			}
 			n.For = token.NoPos
 		case *ast.FuncType:
+			if n == nil {
+				return
+			}
 			if n.Func != token.NoPos {
 				fixed = true
 			}
 			n.Func = token.NoPos
 		case *ast.GenDecl:
+			if n == nil {
+				return
+			}
 			if n.TokPos != token.NoPos {
 				fixed = true
 			}
@@ -189,11 +258,17 @@ func killpos(file *ast.File) bool {
 			}
 			n.Rparen = token.NoPos
 		case *ast.GoStmt:
+			if n == nil {
+				return
+			}
 			if n.Go != token.NoPos {
 				fixed = true
 			}
 			n.Go = token.NoPos
 		case *ast.Ident:
+			if n == nil {
+				return
+			}
 			// Keep position information for AST identifiers. Otherwise, selection
 			// expressions get broken into two lines. E.g.
 			//
@@ -205,21 +280,33 @@ func killpos(file *ast.File) bool {
 			//}
 			//n.NamePos = token.NoPos
 		case *ast.IfStmt:
+			if n == nil {
+				return
+			}
 			if n.If != token.NoPos {
 				fixed = true
 			}
 			n.If = token.NoPos
 		case *ast.ImportSpec:
+			if n == nil {
+				return
+			}
 			if n.EndPos != token.NoPos {
 				fixed = true
 			}
 			n.EndPos = token.NoPos
 		case *ast.IncDecStmt:
+			if n == nil {
+				return
+			}
 			if n.TokPos != token.NoPos {
 				fixed = true
 			}
 			n.TokPos = token.NoPos
 		case *ast.IndexExpr:
+			if n == nil {
+				return
+			}
 			if n.Lbrack != token.NoPos {
 				fixed = true
 			}
@@ -229,26 +316,41 @@ func killpos(file *ast.File) bool {
 			}
 			n.Rbrack = token.NoPos
 		case *ast.InterfaceType:
+			if n == nil {
+				return
+			}
 			if n.Interface != token.NoPos {
 				fixed = true
 			}
 			n.Interface = token.NoPos
 		case *ast.KeyValueExpr:
+			if n == nil {
+				return
+			}
 			if n.Colon != token.NoPos {
 				fixed = true
 			}
 			n.Colon = token.NoPos
 		case *ast.LabeledStmt:
+			if n == nil {
+				return
+			}
 			if n.Colon != token.NoPos {
 				fixed = true
 			}
 			n.Colon = token.NoPos
 		case *ast.MapType:
+			if n == nil {
+				return
+			}
 			if n.Map != token.NoPos {
 				fixed = true
 			}
 			n.Map = token.NoPos
 		case *ast.ParenExpr:
+			if n == nil {
+				return
+			}
 			if n.Lparen != token.NoPos {
 				fixed = true
 			}
@@ -258,6 +360,9 @@ func killpos(file *ast.File) bool {
 			}
 			n.Rparen = token.NoPos
 		case *ast.RangeStmt:
+			if n == nil {
+				return
+			}
 			if n.For != token.NoPos {
 				fixed = true
 			}
@@ -267,21 +372,33 @@ func killpos(file *ast.File) bool {
 			}
 			n.TokPos = token.NoPos
 		case *ast.ReturnStmt:
+			if n == nil {
+				return
+			}
 			if n.Return != token.NoPos {
 				fixed = true
 			}
 			n.Return = token.NoPos
 		case *ast.SelectStmt:
+			if n == nil {
+				return
+			}
 			if n.Select != token.NoPos {
 				fixed = true
 			}
 			n.Select = token.NoPos
 		case *ast.SendStmt:
+			if n == nil {
+				return
+			}
 			if n.Arrow != token.NoPos {
 				fixed = true
 			}
 			n.Arrow = token.NoPos
 		case *ast.SliceExpr:
+			if n == nil {
+				return
+			}
 			if n.Lbrack != token.NoPos {
 				fixed = true
 			}
@@ -291,21 +408,33 @@ func killpos(file *ast.File) bool {
 			}
 			n.Rbrack = token.NoPos
 		case *ast.StarExpr:
+			if n == nil {
+				return
+			}
 			if n.Star != token.NoPos {
 				fixed = true
 			}
 			n.Star = token.NoPos
 		case *ast.StructType:
+			if n == nil {
+				return
+			}
 			if n.Struct != token.NoPos {
 				fixed = true
 			}
 			n.Struct = token.NoPos
 		case *ast.SwitchStmt:
+			if n == nil {
+				return
+			}
 			if n.Switch != token.NoPos {
 				fixed = true
 			}
 			n.Switch = token.NoPos
 		case *ast.TypeAssertExpr:
+			if n == nil {
+				return
+			}
 			if n.Lparen != token.NoPos {
 				fixed = true
 			}
@@ -315,16 +444,25 @@ func killpos(file *ast.File) bool {
 			}
 			n.Rparen = token.NoPos
 		case *ast.TypeSpec:
+			if n == nil {
+				return
+			}
 			if n.Assign != token.NoPos {
 				fixed = true
 			}
 			n.Assign = token.NoPos
 		case *ast.TypeSwitchStmt:
+			if n == nil {
+				return
+			}
 			if n.Switch != token.NoPos {
 				fixed = true
 			}
 			n.Switch = token.NoPos
 		case *ast.UnaryExpr:
+			if n == nil {
+				return
+			}
 			if n.OpPos != token.NoPos {
 				fixed = true
 			}
