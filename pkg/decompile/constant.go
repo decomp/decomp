db2f5706f064101e34080996b6767d7e00b2e336
Robin Eklind <rnd0x00@gmail.com>
2019-01-15 11:41:01 +0100

    decompile: lift IR constants

--- pkg/decompile/constant.go
+++ pkg/decompile/constant.go
@@ -7,7 +7,9 @@ import (
 	"math"
 	"strconv"
 
+	"github.com/llir/llvm/ir"
 	"github.com/llir/llvm/ir/constant"
+	"github.com/pkg/errors"
 )
 
 // liftConst lifts the LLVM IR constant to an equivalent Go expression.
@@ -19,27 +21,154 @@ func (gen *Generator) liftConst(irConst constant.Constant) (ast.Expr, error) {
 		return gen.liftIntConst(irConst), nil
 	case *constant.Float:
 		return gen.liftFloatConst(irConst), nil
-	//case *constant.Null:
+	case *constant.Null:
+		return ast.NewIdent("nil"), nil
 	//case *constant.None:
 	// Complex constants
-	//case *constant.Struct:
-	//case *constant.Array:
-	//case *constant.CharArray:
-	//case *constant.Vector:
-	//case *constant.ZeroInitializer:
+	case *constant.Struct:
+		goType, err := gen.goType(irConst.Type())
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		var fields []ast.Expr
+		for _, irField := range irConst.Fields {
+			field, err := gen.liftConst(irField)
+			if err != nil {
+				return nil, errors.WithStack(err)
+			}
+			fields = append(fields, field)
+		}
+		return &ast.CompositeLit{
+			Type: goTypeExpr(goType),
+			Elts: fields,
+		}, nil
+	case *constant.Array:
+		goType, err := gen.goType(irConst.Type())
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		var elems []ast.Expr
+		for _, irElem := range irConst.Elems {
+			elem, err := gen.liftConst(irElem)
+			if err != nil {
+				return nil, errors.WithStack(err)
+			}
+			elems = append(elems, elem)
+		}
+		return &ast.CompositeLit{
+			Type: goTypeExpr(goType),
+			Elts: elems,
+		}, nil
+	case *constant.CharArray:
+		return &ast.BasicLit{
+			Kind:  token.STRING,
+			Value: string(irConst.X),
+		}, nil
+	case *constant.Vector:
+		goType, err := gen.goType(irConst.Type())
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		var elems []ast.Expr
+		for _, irElem := range irConst.Elems {
+			elem, err := gen.liftConst(irElem)
+			if err != nil {
+				return nil, errors.WithStack(err)
+			}
+			elems = append(elems, elem)
+		}
+		return &ast.CompositeLit{
+			Type: goTypeExpr(goType),
+			Elts: elems,
+		}, nil
+	case *constant.ZeroInitializer:
+		goType, err := gen.goType(irConst.Type())
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		typ := goTypeExpr(goType)
+		return &ast.StarExpr{
+			X: &ast.CallExpr{
+				Fun:  ast.NewIdent("new"),
+				Args: []ast.Expr{typ},
+			},
+		}, nil
 	// Global variable and function addresses
-	//case *ir.Global:
-	//case *ir.Function:
-	//case *ir.Alias:
-	//case *ir.IFunc:
+	case *ir.Global:
+		return ast.NewIdent(irConst.Name()), nil
+	case *ir.Func:
+		return ast.NewIdent(irConst.Name()), nil
+	case *ir.Alias:
+		return ast.NewIdent(irConst.Name()), nil
+	case *ir.IFunc:
+		return ast.NewIdent(irConst.Name()), nil
 	// Undefined values
-	//case *constant.Undef:
+	case *constant.Undef:
+		// TODO: figure out better representation for undefined values.
+		return ast.NewIdent("nil"), nil
 	// Addresses of basic blocks
 	//case *constant.BlockAddress:
 	// Constant expressions
-	//case constant.Expression:
+	case constant.Expression:
+		return gen.liftConstExpr(irConst)
 	default:
 		panic(fmt.Errorf("support for constant %T not yet implemented", irConst))
+	}
+}
+
+// liftConstExpr lifts the LLVM IR constant expression to an equivalent Go
+// expression.
+func (gen *Generator) liftConstExpr(irConst constant.Constant) (ast.Expr, error) {
+	switch irConst := irConst.(type) {
+	// Binary expressions
+	//case *constant.ExprAdd:
+	//case *constant.ExprFAdd:
+	//case *constant.ExprSub:
+	//case *constant.ExprFSub:
+	//case *constant.ExprMul:
+	//case *constant.ExprFMul:
+	//case *constant.ExprUDiv:
+	//case *constant.ExprSDiv:
+	//case *constant.ExprFDiv:
+	//case *constant.ExprURem:
+	//case *constant.ExprSRem:
+	//case *constant.ExprFRem:
+	// Bitwise expressions
+	//case *constant.ExprShl:
+	//case *constant.ExprLShr:
+	//case *constant.ExprAShr:
+	//case *constant.ExprAnd:
+	//case *constant.ExprOr:
+	//case *constant.ExprXor:
+	// Vector expressions
+	//case *constant.ExprExtractElement:
+	//case *constant.ExprInsertElement:
+	//case *constant.ExprShuffleVector:
+	// Aggregate expressions
+	//case *constant.ExprExtractValue:
+	//case *constant.ExprInsertValue:
+	// Memory expressions
+	//case *constant.ExprGetElementPtr:
+	// Conversion expressions
+	//case *constant.ExprTrunc:
+	//case *constant.ExprZExt:
+	//case *constant.ExprSExt:
+	//case *constant.ExprFPTrunc:
+	//case *constant.ExprFPExt:
+	//case *constant.ExprFPToUI:
+	//case *constant.ExprFPToSI:
+	//case *constant.ExprUIToFP:
+	//case *constant.ExprSIToFP:
+	//case *constant.ExprPtrToInt:
+	//case *constant.ExprIntToPtr:
+	//case *constant.ExprBitCast:
+	//case *constant.ExprAddrSpaceCast:
+	// Other expressions
+	//case *constant.ExprICmp:
+	//case *constant.ExprFCmp:
+	//case *constant.ExprSelect:
+	default:
+		panic(fmt.Errorf("support for constant expression %T not yet implemented", irConst))
 	}
 }
 
