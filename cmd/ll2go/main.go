5028d6c9f68a74d05e40008eace94ad1c3b4c0b8
Haoxi Tan <haoxi.tan@gmail.com>
2021-11-05 13:48:34 +1000

    handle nil constants from llvm code (testing with retdec llvm ir

--- cmd/ll2go/main.go
+++ cmd/ll2go/main.go
@@ -350,7 +350,21 @@ func (d *decompiler) pointerToConst(c constant.Constant) ast.Expr {
 	// Constant expressions
 	case constant.Expression:
 		return d.expr(c)
+
 	default:
+		// handle nil constants 
+		// example error: panic: support for value <nil> not yet implemented
+		if c == nil {
+			
+			// return d.value(c)
+			// return d.constant(constant.Constant(0));
+
+			// make a new integer with value 0 for this
+			return d.value(constant.NewInt(irtypes.I32, 0))
+
+
+		}
+		// fmt.Println("Constant: %v", c)
 		panic(fmt.Sprintf("support for value %T not yet implemented", c))
 	}
 }
